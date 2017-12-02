eda475953f5f72b013c4a338afba8ed3b1080653
Rene <rene@alvaroquintana.com>
2017-12-02 17:18:07 -0600

    Esta es otra prueba para el git

---
+++ test/src/test/Main.java
@@ -0,0 +1,9 @@
+package test;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Esta es una prueba para verificar el control de versiones con Github");
+	}
+
+}
